09572502a450dfbd60e3bf793f0ce64bab99437b
Kelley Yonezaki <kelleysy@gmail.com>
2017-01-28 18:52:02 -0800

    refactor, add MainContainer

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
 function StartOver () {
 	return (
@@ -16,30 +17,35 @@ function StartOver () {
 }
 
 function Results (props) {
+	if (props.isLoading === true) {
+		return (
+			<p> LOADING </p>
+		)
+	}
 	if (props.scores[0] === props.scores[1]) {
 		return (
-				<div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-					<h1>It's a tie!</h1>
-					<StartOver />
-				</div>			
-			)
+			<MainContainer>
+				<h1>It's a tie!</h1>
+				<StartOver />
+			</MainContainer>			
+		)
 	}
 	var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
 	var losingIndex = winningIndex === 0 ? 1 : 0;
 	return (
-			<div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-				<h1>Results</h1>
-				<div className='col-sm-8 col-sm-offset-2'>
-					<UserDetailsWrapper header='Winner'>
-						<UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
-					</UserDetailsWrapper>
-					<UserDetailsWrapper header='Loser'>
-						<UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
-					</UserDetailsWrapper>
-				</div>
-				<StartOver />
+		<MainContainer>
+			<h1>Results</h1>
+			<div className='col-sm-8 col-sm-offset-2'>
+				<UserDetailsWrapper header='Winner'>
+					<UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+				</UserDetailsWrapper>
+				<UserDetailsWrapper header='Loser'>
+					<UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+				</UserDetailsWrapper>
 			</div>
-		)
+			<StartOver />
+		</MainContainer>
+	)
 }
 
 Results.propTypes = {
